d7e112b3ac4dbca5412bd8c654834dabc12a5222
Jakub Perzylo <jakub.perzylo@swmansion.com>
2021-04-19 15:14:13 +0200

    Added object detection and frontend layout

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, Response
 import cv2
+from sobel_decorator import SobelDecorator
+from object_detection_decorator import ObjectDetectionDecorator 
+import time
 
 app = Flask(__name__, template_folder = "", static_folder="")
 cap = cv2.VideoCapture(0)
@@ -16,27 +19,28 @@ def add_header(response):
 def index():
     return render_template("index.html")
 
-@app.route("/video")
-def video():
-    return Response(frames(), mimetype="multipart/x-mixed-replace; boundary=frame")
+@app.route("/video/<decorator>")
+def video(decorator):
+    return Response(frames(apply_decorator(decorator)), mimetype="multipart/x-mixed-replace; boundary=frame")
 
-def frames():
+def frames(decorator):
+    counter = 1
     while True:
-        ret, frame = cap.read()
-        blur = cv2.GaussianBlur(frame, (3, 3), 0)
-        gray = cv2.cvtColor(blur, cv2.COLOR_BGR2GRAY)
-        grad_x = cv2.Sobel(gray, cv2.CV_16S, 1, 0)
-        grad_x = cv2.convertScaleAbs(grad_x)
-        grad_y = cv2.Sobel(gray, cv2.CV_16S, 0, 1)
-        grad_y = cv2.convertScaleAbs(grad_y)
-        grad = cv2.addWeighted(grad_x, 0.5, grad_y, 0.5, 0)
-            
-        _, jpg = cv2.imencode('.jpg', grad, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
-        jpg = jpg.tobytes()
+        _, frame = cap.read()
+        
+        data = decorator.decorate(frame)
+        counter += 1
             
         yield (b"--frame\r\n" +
-               b"Content-Type: image/jpeg\r\n\r\n" + jpg + b"\r\n")
+               b"Content-Type: image/jpeg\r\n\r\n" + data + b"\r\n")
 
+
+def apply_decorator(decorator):
+    if decorator == "detection":
+        return ObjectDetectionDecorator()
+    if decorator == "sobel":
+        return SobelDecorator()
+    raise RuntimeError(f"Could not find filter name: {filter}") 
 
 if __name__ == "__main__":
     app.run(debug = True)
